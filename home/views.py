41da495a7420969e9e0cd0c60a4e95b1acc63e2a
Sinan URUN <sinanurun24@gmail.com>
2024-11-02 16:53:42 +0300

    settings bilgileri uerda sunuldu

--- home/views.py
+++ home/views.py
@@ -1,14 +1,19 @@
+
 from django.http import HttpResponse
 from django.shortcuts import render
+
+from home.models import Setings
+
 
 # Create your views here.
 
 def index(request):
-
+    settings = Setings.objects.get(pk=1)
     metin = "BTK Kursiyerleri"
     # return HttpResponse("Hello, %s. <br>You're at the" % metin)
     # return HttpResponse(metin)
-    context = {"sayfa":"home"}
+    context = {"sayfa":"home",
+               'settings':settings}
 
     # return render(request, 'index.html',context)
     return render(request, 'index.html', context)
