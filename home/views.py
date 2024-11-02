80ec823eb0202d26fbdc1b710542901c821a9d5a
Sinan URUN <sinanurun24@gmail.com>
2024-11-02 14:33:16 +0300

    ana sayfa slider kontrolü

--- home/views.py
+++ home/views.py
@@ -8,6 +8,6 @@ def index(request):
     metin = "BTK Kursiyerleri"
     # return HttpResponse("Hello, %s. <br>You're at the" % metin)
     # return HttpResponse(metin)
-    context = {"icerik1":metin}
+    context = {"sayfa":"iletisim"}
     # return render(request, 'index.html',context)
-    return render(request, 'index.html')
+    return render(request, 'index.html', context)
