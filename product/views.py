df89bf342001dbce83898bf53df59a0d58efcee3
Sinan URUN <sinanurun24@gmail.com>
2024-11-09 16:19:25 +0300

    yorum yapıldı ve görüntülendi

--- product/views.py
+++ product/views.py
@@ -1,8 +1,9 @@
+from django.contrib import messages
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 from product.forms import SearchForm, CommentForm
-from product.models import Category, Product, Images
+from product.models import Category, Product, Images, Comment
 
 
 # Create your views here.
@@ -24,8 +25,9 @@ def productDetail(request,id, slug):
     images = Images.objects.filter(product_id = id)
     urunKategori = Category.objects.get(pk=urun.category_id)
     kategoriurunleri = Product.objects.filter(category_id=urun.category_id)
+    comments = Comment.objects.filter(product_id = id)
     form = CommentForm
-    context = {'urun':urun,'images':images,
+    context = {'urun':urun,'images':images,'comments':comments,
                'urunKategori':urunKategori,
                'kategoriurunleri':kategoriurunleri, 'form':form}
     return render(request,'product_detail.html',context)
@@ -39,3 +41,23 @@ def search(request):
             context = {'results':results}
             return render(request, 'product_search.html', context)
     return HttpResponseRedirect('/')
+
+def addcomment(request,id):
+    url = request.META.get('HTTP_REFERER')
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment()
+            comment.comment = form.cleaned_data['comment']
+            comment.product_id = id
+            comment.user = request.user
+            # comment.user_id = request.user.id
+            comment.subject = form.cleaned_data['subject']
+            comment.rate = int(form.cleaned_data['rate'])
+            comment.ip = request.META.get('REMOTE_ADDR')
+            comment.save()
+            messages.success(request, 'yorum ekleme başarılı')
+            return HttpResponseRedirect(url)
+    else:
+        messages.error(request, 'yorum ekleme Başarısız')
+        return HttpResponseRedirect(url)
