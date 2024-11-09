f5af1caca603bce55334116d0ec93c903ee61cc2
Sinan URUN <sinanurun24@gmail.com>
2024-11-09 14:39:43 +0300

    register tamam
    menü de kullanıcı kontrolü yapıldı

--- user/views.py
+++ user/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import logout, authenticate, login
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
-from user.forms import LoginForm
+from user.forms import LoginForm, RegisterForm
 
 
 # Create your views here.
@@ -27,8 +27,27 @@ def user_login(request):
                 messages.error(request, 'Oturum Açma Başarısız')
                 return HttpResponseRedirect('/user/login')
 
-
-
     form = LoginForm
     context = {'form': form}
     return render(request, 'login.html', context)
+
+def user_register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username').lower()
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'Kayıt Başarılı')
+                return HttpResponseRedirect('/user/register')
+            else:
+                messages.error(request, 'Kayıt Başarısız')
+                return HttpResponseRedirect('/user/register')
+
+
+    form = RegisterForm
+    context = {'form': form}
+    return render(request, 'register.html', context)
