4c12b26d29c0aa7aa0ab5efeb49e35f072e9d9aa
Sinan URUN <sinanurun24@gmail.com>
2024-11-10 15:07:58 +0300

    favoriye ürün ekleme ve çıkartma işlemleri tamamdır

--- order/views.py
+++ order/views.py
@@ -41,3 +41,10 @@ def favorites(request):
     favorites = AddFavorite.objects.filter(user=current_user)
     context = {'favorites':favorites}
     return render(request, 'favorites_products.html',context)
+
+@login_required(login_url='/login')  # Check login
+def delfavorite(request,id):
+    url = request.META.get('HTTP_REFERER')
+    current_user = request.user
+    AddFavorite.objects.filter(product_id=id, user_id=current_user.id).delete()
+    return HttpResponseRedirect(url)
