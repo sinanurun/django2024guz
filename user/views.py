a5aceb09af795b7d985e8807f8b76134334bc0bb
Sinan URUN <sinanurun24@gmail.com>
2024-11-10 10:42:37 +0300

    kullanıcı ve profil bilgileri güncellendi

--- user/views.py
+++ user/views.py
@@ -68,7 +68,16 @@ def user_profile(request):
 
 @login_required(login_url='/user/login/')
 def user_update(request):
-    user_form = UserUpdateForm(instance=request.user)
-    profile_form = ProfileUpdateForm(instance=request.user.userprofile)
-    context = {'user_form': user_form, 'profile_form':profile_form}
-    return render(request, 'user_update.html',context)
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.userprofile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Kullanıcı Güncelleme Başarılı')
+            return HttpResponseRedirect('/user')
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.userprofile)
+        context = {'user_form': user_form, 'profile_form':profile_form}
+        return render(request, 'user_update.html',context)
